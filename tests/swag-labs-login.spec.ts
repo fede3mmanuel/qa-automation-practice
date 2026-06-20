d3031f47af1eb06e42a04bb96f0b87dec0b644dd
Emmanuel Sanchez <fede@fede.website>
2026-06-20 16:45:03 -0600

    Add initial test for Swag Labs login functionality

---
+++ tests/swag-labs-login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('descripcion del test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.fill('[data-test="username"]', 'standard_user');
+  await page.fill('[data-test="password"]', 'secret_sauce');
+  await page.click('[data-test="login-button"]');
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
