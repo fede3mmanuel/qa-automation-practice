8981f94f24ce3ef2273480f671d77461cc740ac2
Emmanuel Sanchez <fede@fede.website>
2026-07-04 22:53:49 -0600

    Add login method to LoginPage class for user authentication

--- tests/pages/LoginPage.ts
+++ tests/pages/LoginPage.ts
@@ -7,4 +7,10 @@ export class LoginPage {
     async goto() {
         await this.page.goto('https://www.saucedemo.com/');
     }
+
+    async login(username: string, password: string) {
+        await this.page.fill('[data-test="username"]', username);
+        await this.page.fill('[data-test="password"]', password);
+        await this.page.click('[data-test="login-button"]');
+    }
 }
