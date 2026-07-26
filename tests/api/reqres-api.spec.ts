c986cc119929d1f55c0e317de883348f3cffd717
Emmanuel Sanchez <fede@fede.website>
2026-07-25 20:25:25 -0600

    get the 200 value successfully

--- tests/api/reqres-api.spec.ts
+++ tests/api/reqres-api.spec.ts
@@ -3,5 +3,9 @@ import { test, expect } from '@playwright/test';
 test.describe('reqres api', () => {
 
     test('Get user 3 returs 200 and has an email', async ({ request }) => {
+        const response = await request.get('https://jsonplaceholder.typicode.com/users/2');
+        expect(response.status()).toBe(200);
 
-})})
+    })
+
+})
