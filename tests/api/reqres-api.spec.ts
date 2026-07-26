74c6ddb01ef4bc29dfd6cb0686613ee51061922d
Emmanuel Sanchez <fede@fede.website>
2026-07-25 20:30:14 -0600

    add email response verification

--- tests/api/reqres-api.spec.ts
+++ tests/api/reqres-api.spec.ts
@@ -6,6 +6,9 @@ test.describe('reqres api', () => {
         const response = await request.get('https://jsonplaceholder.typicode.com/users/2');
         expect(response.status()).toBe(200);
 
+        const jsonResponse = await response.json();
+        expect(jsonResponse.email).toBeDefined();
+
     })
 
 })
