5d74aae25e0a7fee60f71cbb1d3b977a49448353
Emmanuel Sanchez <fede@fede.website>
2026-07-25 20:33:53 -0600

    Fix user ID in jsonplaceholder API test and update test description

--- tests/api/reqres-api.spec.ts
+++ tests/api/reqres-api.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('reqres api', () => {
+test.describe('jsonplaceholder test api', () => {
 
     test('Get user 3 returs 200 and has an email', async ({ request }) => {
-        const response = await request.get('https://jsonplaceholder.typicode.com/users/2');
+        const response = await request.get('https://jsonplaceholder.typicode.com/users/3');
         expect(response.status()).toBe(200);
 
         const jsonResponse = await response.json();
         expect(jsonResponse.email).toBeDefined();
 
-    })
+    });
 
-})
+});
