6e14ea9672cb6f4992a15f888e77e82ff29af601
Emmanuel Sanchez <fede@fede.website>
2026-09-03 21:01:18 -0600

    Refactor accessibility tests to include Inventory and Cart pages, enhancing coverage and structure

--- tests/accessibility/a11y.spec.ts
+++ tests/accessibility/a11y.spec.ts
@@ -1,12 +1,46 @@
 import { test, expect } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright'; // 1
+import AxeBuilder from '@axe-core/playwright';
+import { LoginPage } from '../pages/LoginPage';
+import { CartPage } from '../pages/CartPage';
 
-test.describe('homepage', () => { // 2
-  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/'); // 3
+test.describe('Accessibility tests', () => {
+  test('Home page: Should not have any automatically detectable accessibility issues', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/'); 
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
+    const accessibilityScanResults = await new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']).analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]); // 5
+    expect(accessibilityScanResults.violations).toEqual([]); 
+  });
+
+  test('Inventory page: Should not have any automatically detectable accessibility issues', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']).analyze();
+
+    expect.soft(accessibilityScanResults.violations).toEqual([]); 
+  });
+
+  test('Cart page: Should not have any automatically detectable accessibility issues', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+    const cartPage = new CartPage(page);
+    await cartPage.addToCart('add-to-cart-sauce-labs-backpack');
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+    await page.click('[data-test="shopping-cart-link"]');
+    await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']).analyze();
+
+    expect.soft(accessibilityScanResults.violations).toEqual([]); 
   });
 });
+
+
